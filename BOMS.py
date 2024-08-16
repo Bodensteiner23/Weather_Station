fda133083279f8f9a024f05118f72ae10b0b0a25
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-08-16 12:12:21 +0200

    Changed Script

--- BOMS.py
+++ BOMS.py
@@ -1,26 +1,28 @@
 import pandas as pd
+import os
 
-# Read the CSV files into DataFrames
-df1 = pd.read_csv('bom_Main_PCB.csv')
-df2 = pd.read_csv('bom_Solar_Module.csv')
+# List .csv
+all_files = os.listdir(os.getcwd())    
+csv_files = list(filter(lambda f: f.endswith('.csv') and f != 'Merged_csv.csv', all_files))
 
-# Add a new column to indicate the source
-df1['Source'] = 'Main_PCB'
-df2['Source'] = 'Solar_Module'
+# Merge
+df = pd.concat(map(pd.read_csv, csv_files), ignore_index=True) 
 
-# Concatenate the DataFrames
-df = pd.concat([df1, df2])
+# Reorder Headers
+column_names = list(df.columns.values)
+column_names.pop(4) # Remove LCSC Header
+df = df.reindex(columns=['Value', 'Footprint', 'Quantity', 'Designator'])
 
-# Group by 'Value' and 'Footprint', then sum the 'Quantity' and join the 'Designator'
-df_grouped = df.groupby(['Value', 'Footprint'], as_index=False).agg({'Quantity': 'sum', 'Designator': ','.join})
+df = df.groupby(['Value', 'Footprint']).agg({
+    'Quantity': 'sum',
+    'Designator': lambda x: ','.join(x)
+}).reset_index()
 
-# Sort the DataFrame by 'Designator' alphabetically
-df_grouped = df_grouped.sort_values(by='Designator')
+df = df.sort_values('Designator')
 
-# Drop the 'LCSC Part #' column
-df_grouped = df_grouped.drop(columns=['LCSC Part #'], errors='ignore')
+# print(df)
 
-# Save the result to a new CSV file
-df_grouped.to_csv('Merged_BOM.csv', index=False)
 
-print("The data has been processed, sorted, and saved.")
+# Store in new .csv
+df.to_csv("Merged_csv.csv", index=False)
+print("CSV is combined")
