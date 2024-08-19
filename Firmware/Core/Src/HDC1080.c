be4739de68707c2049fcc065b2d6eb6ef2b46d79
Bodensteiner23 <matthias.bodensteiner@fau.de>
2024-08-19 23:46:50 +0200

    HDC1080 Prototype should be finished.

--- Firmware/Core/Src/HDC1080.c
+++ Firmware/Core/Src/HDC1080.c
@@ -21,29 +21,36 @@ void HDC1080_initSensor(I2C_HandleTypeDef _hi2c) {
 }
 
 
-void HDC1080_readData(I2C_HandleTypeDef _hi2c, uint8_t data_read[4]) {
+ws_value_t HDC1080_readData(I2C_HandleTypeDef _hi2c) {
 
 	uint8_t data_send[1];
+	uint8_t data_read[4];
 	ws_value_t temp_humid_values;
 
 	// Start data pull request
 	HAL_I2C_Mem_Write(&_hi2c, HDC1080_I2C_Address, Temperature_Register_Address,
-			1, data_send, 1, 100);
+			1, data_send, 1, 100); // FixMe: Is it only working when typing: &data_send[0]
 
 	HAL_Delay(15);
 
 	// Read Temp Data
-	uint16_t temp_raw_val = HAL_I2C_Mem_Read(&_hi2c, HDC1080_I2C_Address, Temperature_Register_Address,
+	HAL_I2C_Mem_Read(&_hi2c, HDC1080_I2C_Address, Temperature_Register_Address,
 						1, &data_read[0], 2, 100);
+	uint16_t temp_raw_val = (data_read[0] << 8) | data_read[1];
 	// Read Humid Data
-	uint16_t humid_raw_val = HAL_I2C_Mem_Read(&_hi2c, HDC1080_I2C_Address, Humidity_Register_Address,
+	 HAL_I2C_Mem_Read(&_hi2c, HDC1080_I2C_Address, Humidity_Register_Address,
 						1, &data_read[2], 2, 100);
+	 uint16_t humid_raw_val = (data_read[2] << 8) | data_read[3];
 
 	if ((temp_raw_val | humid_raw_val) == HAL_ERROR) {
 		Error_Handler();			 // Transmission Error
 	}
 
-	temp_humid_values.temp_val = (temp_raw_val << 8) | temp_raw_val;
+	temp_humid_values.temp_val = temp_raw_val >> 2;		// Convert to 14bit. Last two bit
+	temp_humid_values.temp_val = ((temp_humid_values.temp_val / 2^16) * 165) - 40;			// ToDo: Check Result
 
+	temp_humid_values.humid_val = humid_raw_val >> 2;
+	temp_humid_values.humid_val = (temp_humid_values.humid_val / 2^16) * 0b11111111111111;	// ToDo: Check Result
 
+	return temp_humid_values;
 }
